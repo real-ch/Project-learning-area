58f8c86c0ee82b830d525c162242c687e4a72db3
ch <ch-way@139.com>
2023-09-10 16:04:17 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-// tbody = document.querySelector("body");
-// tbody.textContext = "hello world!";
-// console.log(tbody); // <div>...</div>
-
-function addtitle(){
-  // var btnAdd = document.querySelector('#btnAdd');
-  // var tbody = document.querySelector("body");
-  // var title = document.createElement("h1");
-  // var textnode = document.createTexContent('Hello World!');
-  //var body = document.getElementsByTagName('body')[0];
- title.textContent = "Hello world!";
- tbody.appendChild(title) ;
-}
 
 
-btnAdd.addEventListener("click",()=>{
-var btnAdd = Document.querySelect("#btnAdd");
-console.log(btnAdd);
-btnAdd.textContent="hello world!";
-}
-  
-)
 
+const bootstrap = require("bootstrap");
