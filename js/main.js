ade423d2c01eb921df4f736d58665b215f8c84c4
ch <ch-way@139.com>
2023-09-13 19:13:28 +0800

    script

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-import * as bootstrap from '../bootstrap/bootstrap-5.3.1';
+
 // import { AppComponent } from './app.component'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
