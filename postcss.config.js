8c0034bc0bd662a7bae589c4e71d48921abc1821
ch <ch-way@139.com>
2023-09-15 13:48:58 +0800

    1

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,9 @@
-null  
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-nested')
+  ]
+}
+
+module.exports = config
