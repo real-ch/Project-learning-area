6e9104aa2d5aee51f9ef552d961eb204e7a32379
ch <ch-way@139.com>
2023-10-04 13:38:02 +0800

    Pagess
    page

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7583" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61553" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
